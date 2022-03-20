438d72070e4f73eca5314644c40fd8059be371b6
IgorTsoy2022 <97473595+IgorTsoy2022@users.noreply.github.com>
2022-03-20 09:06:50 +0300

    Sprint 5

---
+++ main.cpp
@@ -0,0 +1,111 @@
+#include <locale.h>
+
+#include "log_duration.h"
+#include "paginator.h"
+#include "remove_duplicates.h"
+#include "request_queue.h"
+#include "search_server.h"
+
+using namespace std::string_literals;
+
+void AddDocument(SearchServer& search_server, int document_id,
+    const std::string& document,
+    DocumentStatus status,
+    const std::vector<int>& ratings) {
+    try {
+        search_server.AddDocument(document_id, document,
+            status, ratings);
+    }
+    catch (const std::invalid_argument& e) {
+        std::cout << "Ошибка добавления документа "s
+            << document_id << ": "s << e.what()
+            << std::endl;
+    }
+}
+
+void FindTopDocuments(const SearchServer& search_server,
+    const std::string& raw_query) {
+    std::cout << "Результаты поиска по запросу: "s
+        << raw_query << std::endl;
+    try {
+        for (const Document& document :
+            search_server.FindTopDocuments(raw_query)) {
+            PrintDocument(document);
+        }
+    }
+    catch (const std::invalid_argument& e) {
+        std::cout << "Ошибка поиска: "s << e.what() << std::endl;
+    }
+}
+
+void MatchDocuments(const SearchServer& search_server,
+    const std::string& query) {
+    try {
+        std::cout << "Матчинг документов по запросу: "s
+            << query << std::endl;
+
+        for (const int document_id : search_server) {
+            const auto [words, status] =
+                search_server.MatchDocument(query, document_id);
+            PrintMatchDocumentResult(document_id, words, status);
+        }
+
+    }
+    catch (const std::invalid_argument& e) {
+        std::cout << "Ошибка матчинга документов на запрос "s
+            << query << ": "s << e.what() << std::endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    SearchServer search_server("and with"s);
+
+    AddDocument(search_server, 1, "funny pet and nasty rat"s,
+                DocumentStatus::ACTUAL, {7, 2, 7});
+    AddDocument(search_server, 2, "funny pet with curly hair"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // дубликат документа 2, будет удалён
+    AddDocument(search_server, 3, "funny pet with curly hair"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // отличие только в стоп-словах, считаем дубликатом
+    AddDocument(search_server, 4, "funny pet and curly hair"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // множество слов такое же, считаем дубликатом документа 1
+    AddDocument(search_server, 5,
+                "funny funny pet and nasty nasty rat"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // добавились новые слова, дубликатом не является
+    AddDocument(search_server, 6,
+                "funny pet and not very nasty rat"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // множество слов такое же, как в id 6, несмотря на другой
+    // порядок, считаем дубликатом
+    AddDocument(search_server, 7,
+                "very nasty rat and not very funny pet"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // есть не все слова, не является дубликатом
+    AddDocument(search_server, 8, "pet with rat and rat and rat"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    // слова из разных документов, не является дубликатом
+    AddDocument(search_server, 9, "nasty rat with curly hair"s,
+                DocumentStatus::ACTUAL, {1, 2});
+
+    std::cout << "Before duplicates removed: "s
+         << search_server.GetDocumentCount() << std::endl;
+
+    RemoveDuplicates(search_server);
+
+    std::cout << "After duplicates removed: "s
+         << search_server.GetDocumentCount() << std::endl;
+
+    return 0;
+}
---
+++ remove_duplicates.cpp
@@ -0,0 +1,28 @@
+#include "remove_duplicates.h"
+
+using namespace std::string_literals;
+
+void RemoveDuplicates(SearchServer& search_server) {
+
+    std::vector<int> vec_dublicate_ids;
+    auto it_end = search_server.end();
+    for (auto it1 = search_server.begin(); it1 != it_end; ++it1) {
+        auto it2_begin = it1;
+        ++it2_begin;
+        if (it2_begin != it_end) {
+            for (auto it2 = it2_begin; it2 != it_end; ++it2) {
+                if (search_server.CompareDocumentsWords(*it1, *it2)) {
+                    vec_dublicate_ids.push_back(*it2);
+                    break;
+                }
+            }
+        }
+    }
+
+    for (const auto id : vec_dublicate_ids) {
+        std::cout << "Found duplicate document id "s
+                  << id << std::endl;
+        search_server.RemoveDocument(id);
+    }
+
+}
